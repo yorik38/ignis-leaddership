39de923173ff7a4f1e6f25485d99896bfdc5effc
yorik38 <yorik.tisseau@gmail.com>
2026-09-08 15:05:31 +0100

    Refine AI opportunity journey

--- website_structure/assets/script.js
+++ website_structure/assets/script.js
@@ -355,7 +355,7 @@ function initAiOpportunityTimeline(){
       timeline.classList.add("is-visible");
       observer.disconnect();
     });
-  }, {threshold:0.35});
+  }, {threshold:0.15, rootMargin:"0px 0px -10%"});
   observer.observe(timeline);
 }
 
